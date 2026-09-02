96ac1923e3dccca05e16d1f2959fe44fdafaf0a6
Raju Sakshena <jackmyble@gmail.com>
2026-09-02 17:50:32 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
 export default defineConfig({
-  site: 'https://bharatki-economy.vercel.app',
+  site: 'https://bharatkieconomy.vercel.app/',
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
